966e37943109936e439502b59964f888b582350e
Muhammad Talha Khan <utubefortk@gmail.com>
2026-06-02 01:05:20 +0500

    Update task_provider.dart

---
+++ lib/providers/task_provider.dart
@@ -0,0 +1,3 @@
+/*  Houses the StateNotifier and StateNotifierProvider. This is the Riverpod 
+    equivalent of the Week 6 provider task, offering immutable state updates and better 
+    compile-time safety. */
