3d424e459de65a469f9ed59ba53b541360cc2438
Muhammad Talha Khan <utubefortk@gmail.com>
2026-06-02 01:08:12 +0500

    difference bw provide & riverpod

--- lib/providers/task_provider.dart
+++ lib/providers/task_provider.dart
@@ -1,3 +1,7 @@
 /*  Houses the StateNotifier and StateNotifierProvider. This is the Riverpod 
     equivalent of the Week 6 provider task, offering immutable state updates and better 
-    compile-time safety. */
+    compile-time safety. 
+    DIFFERENCE: Riverpod enforces immutability. Instead of modifying an existing list and yelling 
+    notifyListeners() like we did in the standard Provider, Riverpod requires us to create 
+    a brand new list every time a state changes. It is safer, faster, and highly preferred 
+    in enterprise software. */
